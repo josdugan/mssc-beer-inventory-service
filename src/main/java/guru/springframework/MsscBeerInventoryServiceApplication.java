b60a20767533deb16b46958023dd9590e0fc3d52
josDugan <josephdug@gmail.com>
2020-08-28 18:24:43 -0400

    Fix typo

--- src/main/java/guru/springframework/MsscBeerInventoryServiceApplication.java
+++ src/main/java/guru/springframework/MsscBeerInventoryServiceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MsscBeerInventoryServiceApplication {gigi
+public class MsscBeerInventoryServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsscBeerInventoryServiceApplication.class, args);
